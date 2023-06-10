20089593f9d6e0674876de190e98e5a915e0dd44
brandin chiu <misfitpixel@brandins-MacBook-Pro.local>
2023-06-09 20:11:56 -0400

    removed identity check

--- Entity/Abstraction/BaseUserToken.php
+++ Entity/Abstraction/BaseUserToken.php
@@ -302,7 +302,6 @@ abstract class BaseUserToken implements AccessTokenEntityInterface, RefreshToken
              * TODO: update User::getRoles() to return scopes here
              */
             ->withClaim('scopes', $this->getScopes())
-            ->withClaim('identity', $this->getIdentityId())
             ->getToken($this->jwtConfiguration->signer(), $this->jwtConfiguration->signingKey())
             ;
     }
